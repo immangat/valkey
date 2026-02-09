71fc26b977f92a028ae61cb3f9bd6af2c2165c92
Mangat Toor <immangat@amazon.com>
2026-02-08 18:39:52 -0800

    add inline skip

--- tests/unit/type/hash.tcl
+++ tests/unit/type/hash.tcl
@@ -936,7 +936,7 @@ start_server {tags {"hash"}} {
 
     # On some platforms strtold("+inf") with valgrind returns a non-inf result
     test {HINCRBYFLOAT does not allow NaN or Infinity} {
-            assert_error "*value is NaN or Infinity*" {r hincrbyfloat hfoo field +inf}
-            assert_equal 0 [r exists hfoo]
+        assert_error "*value is NaN or Infinity*" {r hincrbyfloat hfoo field +inf}
+        assert_equal 0 [r exists hfoo]
     } {} {valgrind:skip}
 }
